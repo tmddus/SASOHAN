3141c286022cf3b3cfc315055e008cc47a63aee9
Sooyeon Chae <cotndus3@naver.com>
2019-06-18 07:48:37 +0900

    탭위치 뜨는거 수정,컬러적용

--- app/src/main/java/com/example/sasohan/MainActivity.java
+++ app/src/main/java/com/example/sasohan/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int FRAGMENT2 = 2;
     private final int FRAGMENT3 = 3;
 
-    private ImageButton bt_tab1, bt_tab2,bt_tab3;
+    private ImageButton ibt_tab1,ibt_tab2,ibt_tab3;
+    private TextView tbt_tab1,tbt_tab2,tbt_tab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         // 위젯에 대한 참조
-        bt_tab1 = (ImageButton)findViewById(R.id.ibt_tab1);
-        bt_tab2 = (ImageButton)findViewById(R.id.ibt_tab2);
-        bt_tab3 = (ImageButton)findViewById(R.id.ibt_tab3);
+        ibt_tab1 = (ImageButton)findViewById(R.id.ibt_tab1);
+        ibt_tab2 = (ImageButton)findViewById(R.id.ibt_tab2);
+        ibt_tab3 = (ImageButton)findViewById(R.id.ibt_tab3);
+
+        tbt_tab1 = (TextView)findViewById(R.id.txt_tab1);
+        tbt_tab2 = (TextView)findViewById(R.id.txt_tab2);
+        tbt_tab3 = (TextView)findViewById(R.id.txt_tab3);
 
         // 탭 버튼에 대한 리스너 연결
-        bt_tab1.setOnClickListener(this);
-        bt_tab2.setOnClickListener(this);
-        bt_tab3.setOnClickListener(this);
+        ibt_tab1.setOnClickListener(this);
+        ibt_tab2.setOnClickListener(this);
+        ibt_tab3.setOnClickListener(this);
+        tbt_tab1.setOnClickListener(this);
+        tbt_tab2.setOnClickListener(this);
+        tbt_tab3.setOnClickListener(this);
 
         // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
         callFragment(FRAGMENT1);
@@ -43,16 +51,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.ibt_tab1 :
+            case R.id.ibt_tab1 : case R.id.txt_tab1:
                 // '버튼1' 클릭 시 '프래그먼트1' 호출
                 callFragment(FRAGMENT1);
                 break;
 
-            case R.id.ibt_tab2 :
+            case R.id.ibt_tab2 :case R.id.txt_tab2:
                 // '버튼2' 클릭 시 '프래그먼트2' 호출
                 callFragment(FRAGMENT2);
                 break;
-            case R.id.ibt_tab3 :
+            case R.id.ibt_tab3 :case R.id.txt_tab3:
                 // '버튼2' 클릭 시 '프래그먼트3' 호출
                 callFragment(FRAGMENT3);
                 break;
