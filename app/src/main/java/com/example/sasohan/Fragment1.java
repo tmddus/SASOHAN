26edf8454d0f8a359fa137b0874a7b8964ee7482
Sooyeon Chae <cotndus3@naver.com>
2019-06-17 19:45:33 +0900

    탭 xml

--- app/src/main/java/com/example/sasohan/Fragment1.java
+++ app/src/main/java/com/example/sasohan/Fragment1.java
@@ -1,4 +1,24 @@
 package com.example.sasohan;
 
-public class Fragment1 {
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+    }
+
 }
--- app/src/main/java/com/example/sasohan/MainActivity.java
+++ app/src/main/java/com/example/sasohan/MainActivity.java
@@ -3,42 +3,104 @@ package com.example.sasohan;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView mTextMessage;
+    private final int FRAGMENT1 = 1;
+    private final int FRAGMENT2 = 2;
+    private final int FRAGMENT3 = 3;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private ImageButton bt_tab1, bt_tab2,bt_tab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // 위젯에 대한 참조
+        bt_tab1 = (ImageButton)findViewById(R.id.ibt_tab1);
+        bt_tab2 = (ImageButton)findViewById(R.id.ibt_tab2);
+        bt_tab3 = (ImageButton)findViewById(R.id.ibt_tab3);
+
+        // 탭 버튼에 대한 리스너 연결
+        bt_tab1.setOnClickListener(this);
+        bt_tab2.setOnClickListener(this);
+        bt_tab3.setOnClickListener(this);
+
+        // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
+        callFragment(FRAGMENT1);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ibt_tab1 :
+                // '버튼1' 클릭 시 '프래그먼트1' 호출
+                callFragment(FRAGMENT1);
+                break;
+
+            case R.id.ibt_tab2 :
+                // '버튼2' 클릭 시 '프래그먼트2' 호출
+                callFragment(FRAGMENT2);
+                break;
+            case R.id.ibt_tab3 :
+                // '버튼2' 클릭 시 '프래그먼트3' 호출
+                callFragment(FRAGMENT3);
+                break;
+        }
+    }
+
+    private void callFragment(int frament_no){
+
+        // 프래그먼트 사용을 위해
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (frament_no){
+            case 1:
+                // '프래그먼트1' 호출
+                Fragment1 fragment1 = new Fragment1();
+                transaction.replace(R.id.fragment_container, fragment1);
+                transaction.commit();
+                break;
+
+            case 2:
+                // '프래그먼트2' 호출
+                Fragment2 fragment2 = new Fragment2();
+                transaction.replace(R.id.fragment_container, fragment2);
+                transaction.commit();
+                break;
+
+            case 3:
+                // '프래그먼트2' 호출
+                Fragment3 fragment3 = new Fragment3();
+                transaction.replace(R.id.fragment_container, fragment3);
+                transaction.commit();
+                break;
+        }
+
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//    }
+
 }
+
+
+
+
