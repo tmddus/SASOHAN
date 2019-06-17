9954fcc0c6004d7150f4bb7a24e6ecaba4b16c32
Seungyeon Song <song99pro@gmail.com>
2019-06-17 14:49:44 +0900

    strings 수정

--- app/src/main/java/com/example/sasohan/MainActivity.java
+++ app/src/main/java/com/example/sasohan/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_sarang:
+                    mTextMessage.setText(R.string.title_sarang);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_sojung:
+                    mTextMessage.setText(R.string.title_sojung);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_giuk:
+                    mTextMessage.setText(R.string.title_giuk);
                     return true;
             }
             return false;
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation =findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
