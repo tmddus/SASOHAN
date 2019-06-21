122aa907ff5e4b35a49943aaa5bc922837d19185
송승연 <wnzk413@gmail.com>
2019-06-21 16:00:40 +0900

    레이아웃 수정(fragment3 수정 필요)

--- app/src/main/java/com/example/sasohan/DB/DBHelper.java
+++ app/src/main/java/com/example/sasohan/DB/DBHelper.java
@@ -62,7 +62,8 @@ public class DBHelper extends SQLiteOpenHelper {
         String token = rndString(10);
 
 
-        db.execSQL("INSERT INTO diary (token, title, content, writedate, receiver, issended, isprivate, receivedate) values(" + token +
+        db.execSQL("INSERT INTO diary (token, title, content, writedate, " +
+                        "receiver, issended, isprivate, receivedate) values(" + token +
                     ", " + title + ", " + content + ", date('now')" + receiver + "," + isSend + "," +  isPrivate + ");"
                 );
     }
@@ -79,6 +80,15 @@ public class DBHelper extends SQLiteOpenHelper {
         );
     }
 
+    public void famillyInsert(String name, String phoneNum, String birth){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("INSERT INTO familly (date, name) values(" +
+                name + "," + phoneNum + "," + birth +
+                ");"
+        );
+
+    }
+
 
     private String rndString(int len){
         Random rnd =new Random();
@@ -86,18 +96,14 @@ public class DBHelper extends SQLiteOpenHelper {
         StringBuffer buf =new StringBuffer();
 
         for(int i=0;i<len;i++) {
-
-            // rnd.nextBoolean() 는 랜덤으로 true, false 를 리턴. true일 시 랜덤 한 소문자를, false 일 시 랜덤 한 숫자를 StringBuffer 에 append 한다.
+            // rnd.nextBoolean() 는 랜덤으로 true, false 를 리턴. true일 시 랜덤 한 소문자를,
+            // false 일 시 랜덤 한 숫자를 StringBuffer 에 append 한다.
 
             if (rnd.nextBoolean()) {
-
                 buf.append((char) ((int) (rnd.nextInt(26)) + 97));
-
             } else {
-
                 buf.append((rnd.nextInt(10)));
             }
-
         }
 
         return buf.toString();
